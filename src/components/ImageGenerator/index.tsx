b5913cefbf0fdafa08f5690d9b7fe17eaaae422b
kroongft <krooooong@naver.com>
2023-09-04 17:42:48 +0900

    add: 폰트 선택 라디오버튼 추가

--- src/components/ImageGenerator/index.tsx
+++ src/components/ImageGenerator/index.tsx
@@ -1,11 +1,20 @@
+import {
+  FormControl,
+  FormControlLabel,
+  FormLabel,
+  Radio,
+  RadioGroup,
+} from "@mui/material";
 import html2canvas from "html2canvas";
 import React, { useState } from "react";
 import { ColorResult, SketchPicker } from "react-color";
 import { styled } from "styled-components";
+import { FONT_LIST } from "../../constants/font";
 
 export const ImageGenerator = () => {
   const [color, setColor] = useState<string>("#111");
   const [text, setText] = useState<string>("텍스트");
+  const [font, setFont] = useState<string>(FONT_LIST[0].value);
 
   const handleChangeColor = (color: ColorResult) => {
     setColor(color.hex);
@@ -36,15 +45,37 @@ export const ImageGenerator = () => {
     <ImageGeneratorContainer>
       <GeneratorWrapper>
         <TextGenerator>
-          <Result id="result" color={color}>
-            {text}
-          </Result>
+          <ResultWrapper>
+            <Result id="result" color={color}>
+              <TextWrapper font={font}>{text}</TextWrapper>
+            </Result>
+          </ResultWrapper>
           <InputWrppaer>
             <input value={text} onChange={handleChangeText} />
           </InputWrppaer>
         </TextGenerator>
+        <FontSelectorWrapper>
+          <FormControl>
+            <FormLabel id="demo-radio-buttons-group-label">폰트</FormLabel>
+            <RadioGroup
+              aria-labelledby="demo-radio-buttons-group-label"
+              defaultValue={font}
+              name="radio-buttons-group"
+            >
+              {FONT_LIST.map((font) => (
+                <FormControlLabel
+                  value={font.value}
+                  key={font.value}
+                  control={<Radio onChange={(e) => setFont(e.target.value)} />}
+                  label={font.label}
+                />
+              ))}
+            </RadioGroup>
+          </FormControl>
+        </FontSelectorWrapper>
         <SketchPicker color={color} onChangeComplete={handleChangeColor} />
       </GeneratorWrapper>
+
       <Button onClick={handleSave}>이미지 생성</Button>
     </ImageGeneratorContainer>
   );
@@ -61,8 +92,9 @@ const ImageGeneratorContainer = styled.div`
 
 const GeneratorWrapper = styled.div`
   display: flex;
+  align-items: flex-end;
   flex-direction: row;
-  gap: 24px;
+  gap: 56px;
 `;
 
 const TextGenerator = styled.div`
@@ -71,20 +103,33 @@ const TextGenerator = styled.div`
   justify-content: end;
 `;
 
+const ResultWrapper = styled.div`
+  border: 1px solid #9b9b9b;
+  border-radius: 8px;
+`;
+
 const Result = styled.div`
   width: 150px;
   height: 150px;
   padding: 8px;
   text-align: justify;
   color: ${(props) => props.color};
-  border: 1px solid #9b9b9b;
-  border-radius: 8px;
   display: flex;
   align-items: center;
   justify-content: center;
   box-sizing: border-box;
+  overflow: hidden;
 `;
 
+const TextWrapper = styled.div<{ font: string }>`
+  font-weight: 900;
+  font-size: 40px;
+  width: 10em;
+  text-align: center;
+  font-family: ${(props) => props.font};
+`;
+
+const FontSelectorWrapper = styled.div``;
 const InputWrppaer = styled.div`
   margin-top: 24px;
 `;
---
+++ src/constants/font.js
@@ -0,0 +1,6 @@
+export const FONT_LIST = [
+  { label: "나눔고딕", value: "NanumSquareNeo" },
+  { label: "평창평화체", value: "PyeongChangPeace-Bold" },
+  { label: "둥근모꼴", value: "DungGeunMo" },
+  { label: "조선궁서체", value: "ChosunGs" },
+];
